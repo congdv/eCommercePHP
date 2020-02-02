3390c2450b98a46aed1c365a0bef5bd9deed6554
ashna <amehta0082@conestogac.on.ca>
2020-02-02 15:40:27 -0500

    created view all products api.

--- api/product/index.php
+++ api/product/index.php
@@ -6,8 +6,48 @@ header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 
+define('TABLE', 'product');
+
+# Database Connection
+include "../../config/database.php";
+
+$verb = strtolower($_SERVER['REQUEST_METHOD']);
+if($verb == 'get') {
+    $temp =array();
+   $temp = getProducts();   
+   
+} 
+else {
+    http_response_code("403");
+    echo '{}';
+}
+
 # Read all products in database
+function getProducts()
+{
+    $database = new Database();
+    $dbConn = $database->getConnection();
+    $cmd = 'SELECT * FROM '.TABLE;
+    $sql = $dbConn->prepare($cmd);
+    $sql->execute();
+    $dataArray = array();
+    while($data = $sql->fetch(PDO::FETCH_ASSOC))
+    {
+        array_push($dataArray,$data);
+        //$dataArray += $data;
+    }
+    return $dataArray;
+}
 
 # Sending back to client
-echo "{}"
+if(!isset($temp)) 
+    {
+        echo "{}";
+    }
+    else
+    {
+        $resp = new stdclass();
+        $resp->products = $temp;
+        echo(json_encode($resp));
+    }
 ?>
