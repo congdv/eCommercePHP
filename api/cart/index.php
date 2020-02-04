3c2f4b419bf985f252a308045c70ad0045541651
chashamdeep kaur <ckaur8336@conestogac.on.ca>
2020-02-03 23:15:18 -0500

    changes commit

--- api/cart/index.php
+++ api/cart/index.php
@@ -7,7 +7,7 @@ header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 
 #ecommerce database connection
-// include "../../config/database.php";
+include "../../confid/database.php";
 
 # Root Path
 include('../../root.php');
@@ -16,7 +16,12 @@ include(HELPER_PATH."/utilsHelper.php");
 include(HELPER_PATH."/authenticationHelper.php");
 
 define('CART', 'cart');
+define('COLUMNS', 'CartID','UserID');
+
 define('CART_DETAILS', 'cart_details');
+define('COLUMNS', 'CartDetailsID','ProductID', 'Quantities', 'CartID');
+
+
 
 # Require Authentication first
 $token = getTokenFromAuthorizationHeader();
@@ -33,69 +38,50 @@ if(!$user) {
 }
 
 # Get all products from cart
-$verb = strtolower($_SERVER['REQUEST_METHOD']);
-if($verb == 'get'){
-    try
-    {
-        $cartProducts = userCart($user); 
-
-        # Sending back to client
-        echo (json_encode ($cartProducts));
-    }
-    catch(Exception $e)
-    {
-        http_response_code(401);
-        $resp = new stdClass();
-        $resp->error = "No Data";
-        $resp->message = "No product select.";
-        echo json_encode($resp);
-    }
-        
+$verb = strtolower($_SERVER['REQUEST_METHOD]']);
+if($verb == 'post'){
+    userCart();
 }else{
     http_response_code("403");
     echo '{}';
-}
+
 
 # Read all purchased that the user bought it
-function userCart($user){
+function userCart(){
     try{
-        $database = new Database();
-        $dbConn = $database->getConnection();
-
-        //get cartID for user
-        $cmd = 'SELECT * FROM '.CART.' WHERE UserID = '.$user['ID'];
-        $sql = $dbConn->prepare($cmd);
-        $sql->execute();
-        
-        //return a single row
-        $cartID = $sql->fetch(PDO::FETCH_ASSOC);
-
-        //get product details for cartID assigned to a user
-        $cmdjoin = 'SELECT * FROM '.CART.' INNER JOIN '.CART_DETAILS.' ON '.CART.'.CartID = '. CART_DETAILS.'.CartID 
-                    WHERE '.CART.'.CartID = '. $cartID['CartID'];
-        $sql = $dbConn->prepare($cmdjoin);
-        $sql->execute();
-
-        $dataArray =array();
-        while($data = $sql->fetch(PDO::FETCH_ASSOC))
-        {
-            $data =  array(
-                'cartDetailsID' => $data['CartDetailsID'],
-                'cartID' => $data['CartID'],
-                'productID' => $data['ProductID'],
-                'quantities' => $data['Quantities']);
-                array_push($dataArray,$data);
-            }
-            return $dataArray;
+        $data = json_decode(trim(file_get_contents("php://input")),true);
+        if(isUserValid($data)){
+            getUserCart($data);
+        }else{
+            throw new Exception("Invalid User");
+        }
     }
     catch(Exception $e){
         http_response_code(400);
         $error = new stdClass();
         $error->error = "Request Failed";
-        $error->message = $e->getMessage();
+        $resp->message = $e->getMessage();
         echo json_encode($error);
         return;
     }
 }
 
+function isUserValid($user){
+    return isset($user['userID'])
+}
+
+function getUserCart($user){
+    $database = new Database();
+    $dbConn = $database->getConnection();
+    $cmd = 'SELECT * FROM'.CART.
+            'WHERE UserID ='. $user['userID'] AND . 'CART' ;
+    $sql = $dbConn->prepare($cmd);
+    $sql->execute();
+            //SELECT * FROM cart AS c
+	//INNER JOIN cart_details AS cd ON cd.CartID = c.CartID;
+
+}
+
+# Sending back to client
+echo "{}";
 ?>
